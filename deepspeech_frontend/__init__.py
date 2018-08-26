b046398fe05e93ec95c5663ddc02a07c0a1abe92
Dan <dan@acceleratenetworks.com>
2018-08-26 16:33:52 -0700

    It works! You can post a file to this and get text back

--- deepspeech_frontend/__init__.py
+++ deepspeech_frontend/__init__.py
@@ -1,6 +1,8 @@
 import os
 import ffmpeg
 import uuid
+from deepspeech.model import Model
+import scipy.io.wavfile as wav
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
@@ -9,6 +11,7 @@ ALLOWED_EXTENSIONS = set(['wav', 'mp3', 'flac'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ds = Model('models/output_graph.pb', 26, 9, 'models/alphabet.txt', 500)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -37,26 +40,31 @@ def upload_file():
                                     filename=convertedFile))
     return '''
     <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
+    <center><title>Upload an Audio File</title>
+    <h1>Upload your audio file</h1>
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
+    <br>
+    <p>We accept wav, mp3 and flac, after uploading you'll be redirected to a transcribed version of your file.</p>
+    <p>No state is retained by this application, we use Mozilla's DeepSpeech library with their default models and settings,
+    To contribute, come check out our code here! We're also looking for suggestions as how to improve accuracy.</p></center>
     '''
 
-@app.route('/uploads/<filename>')
+@app.route('/results/<filename>')
 def transcribe(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    fs, audio = wav.read(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    processed_data = ds.stt(audio, fs)
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return processed_data
 
 # Use ffmpeg to convert our file to WAV @ 16k
 def normalize_file(file):
     filename = str(uuid.uuid4()) + ".wav"
     fileLocation = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     stream = ffmpeg.input(file)
-    stream = ffmpeg.output(stream, fileLocation, format='s16le', acodec='pcm_s16le', ac=1, ar='16k')
+    stream = ffmpeg.output(stream, fileLocation, acodec='pcm_s16le', ac=1, ar='16k')
     # stream = ffmpeg.overwrite_output(stream)
     ffmpeg.run(stream)
-    print(filename)
     return filename
