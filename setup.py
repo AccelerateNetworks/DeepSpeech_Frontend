1c908e1c34a2922d8845a07965ae44a322368030
Dan Ryan <dan@acceleratenetworks.com>
2020-03-29 15:10:54 -0700

    Added dependencies

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from distutils.core import setup
 
 setup(name='deepspeech-frontend',
-      version="0.1.0",
+      version="0.6.1",
       description='Recieves API calls and returns text.',
       author='Dan Ryan',
       author_email='dan@acceleratenetworks.com',
       url='https://git.callpipe.com/fusionpbx/deepspeech_frontend',
       packages=['deepspeech_frontend'],
-      install_requires=["flask"])
+      install_requires=['ffmpeg-python', 'flask', 'deepspeech', 'uuid', 'requests', 'scipy'])
